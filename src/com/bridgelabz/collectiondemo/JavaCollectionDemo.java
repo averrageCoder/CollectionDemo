7125aba8016ec808a3f23713a2f93f512fbc26d3
Harsh R Jain <rjainharsh@gmail.com>
2021-09-11 17:09:26 +0530

    ADDED: priority queue functionalities to add, peek, remove, poll and print queue

--- src/com/bridgelabz/collectiondemo/JavaCollectionDemo.java
+++ src/com/bridgelabz/collectiondemo/JavaCollectionDemo.java
@@ -3,37 +3,39 @@ package com.bridgelabz.collectiondemo;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.Stack;
 
 public class JavaCollectionDemo {
 
 	public static void main(String[] args) {
-		
+
 		doListDemo();
 		doStackDemo();
+		doQueueDemo();
 
 	}
 
 	private static void doListDemo() {
-		
+
 		System. out.println("In doListDemo") ;
 		//Creating List
 		List<String> list = new LinkedList<>();
 		//Adding object to the list
-		
+
 		list.add("Ravi");
 		list.add("Vijay");
 		list.add("Ravi");
 		list.add("Ajay");
-		
+
 		//Traversing list through Iterator
 		Iterator itr=list.iterator();
 		while(itr.hasNext()){
 			System.out.println(itr.next());
 		}
-		
+
 	}
-	
+
 	private static void doStackDemo() {
 		System.out.println("\nIn doStackDemo") ;
 		Stack<String> stack = new Stack<>();
@@ -42,12 +44,36 @@ public class JavaCollectionDemo {
 		stack.push("Amit");
 		stack.push("Ashish");
 		stack.push("Garima");
-		
+
 		String pop = stack.pop();
-		
+
 		Iterator itr=stack.iterator();
 		while(itr.hasNext()){
 			System.out.println(itr.next());
+		}
+	}
+
+	private static void doQueueDemo() {
+		System.out.println("\nIn doQueueDemo") ;
+		PriorityQueue<String> queue = new PriorityQueue<>();
+		queue.add("Amit Sharma");
+		queue.add("Vijay Raj");
+		queue.add("JaiShankar") ;
+		queue.add("Raj");
+		System.out.println("Head: "+queue.element());
+		System.out.println("Head: "+queue. peek());
+		System.out.println("Iterating the queue elements:");
+		Iterator itr=queue.iterator();
+		while(itr.hasNext()){
+			System.out.println(itr.next());
+		}
+		queue.remove();
+		queue.poll();
+		System.out.println("\nAfter removing two elements:");
+		
+		Iterator<String> itr2=queue.iterator();
+		while(itr2.hasNext()){
+			System.out.println(itr2.next());
 		}
 	}
 
