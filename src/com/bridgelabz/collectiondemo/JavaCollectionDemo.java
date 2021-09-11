e8b8a139be89d6180b7f2a3b490178843b6bf588
Harsh R Jain <rjainharsh@gmail.com>
2021-09-11 16:58:13 +0530

    ADDED: stack functionalities to push, pop and print stack

--- src/com/bridgelabz/collectiondemo/JavaCollectionDemo.java
+++ src/com/bridgelabz/collectiondemo/JavaCollectionDemo.java
@@ -3,12 +3,14 @@ package com.bridgelabz.collectiondemo;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 public class JavaCollectionDemo {
 
 	public static void main(String[] args) {
 		
 		doListDemo();
+		doStackDemo();
 
 	}
 
@@ -30,6 +32,23 @@ public class JavaCollectionDemo {
 			System.out.println(itr.next());
 		}
 		
+	}
+	
+	private static void doStackDemo() {
+		System.out.println("\nIn doStackDemo") ;
+		Stack<String> stack = new Stack<>();
+		stack.push("Ayush");
+		stack.push("Garvit");
+		stack.push("Amit");
+		stack.push("Ashish");
+		stack.push("Garima");
+		
+		String pop = stack.pop();
+		
+		Iterator itr=stack.iterator();
+		while(itr.hasNext()){
+			System.out.println(itr.next());
+		}
 	}
 
 }
